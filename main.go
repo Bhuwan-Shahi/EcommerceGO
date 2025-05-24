7d10bcd5b7c040e43d33f646f7cd043950a5dc7d
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2025-05-24 18:26:37 +0545

    GOFIBER

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("Hello, From EcommerceGO")
+	app := fiber.New()
+
+	app.Listen("localhost:3000")
+	fmt.Println("Server is running on http://localhost:3000")
 }
