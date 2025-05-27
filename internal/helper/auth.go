bdbfe79bd3b5bd2a6f38726cea8fcc622574bc5b
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2025-05-27 15:24:33 +0545

    jwt token verificaiton

---
+++ internal/helper/auth.go
@@ -0,0 +1,121 @@
+package helper
+
+import (
+	"ecommerceGO/internal/domain"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Auth struct {
+	Secret string
+}
+
+func (a Auth) CreateHashedPassword(p string) (string, error) {
+
+	if len(p) < 6 {
+		return "", errors.New("Password should be more the 6 character")
+	}
+
+	hashP, err := bcrypt.GenerateFromPassword([]byte(p), 10)
+
+	if err != nil {
+		//log actual error and pass to loggin tool
+
+		return "", errors.New("Password hashing failed")
+
+	}
+
+	return string(hashP), nil
+
+}
+
+func (a Auth) GenerateToken(id uint, email string, role string) (string, error) {
+	if id == 0 || email == "" || role == "" {
+		return "", errors.New("required inputs are missing")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": id,
+		"email":   email,
+		"role":    role,
+		"exp":     time.Now().Add(time.Hour * 24 * 30).UTC().Unix(),
+	})
+
+	tokenStr, err := token.SignedString([]byte(a.Secret))
+
+	if err != nil {
+		return "", errors.New("unable to sign a error")
+	}
+	return tokenStr, nil
+
+}
+
+func (a Auth) VerifyPassword(pP, hP string) error {
+
+	if len(pP) < 6 {
+		return errors.New("Password should be more the 6 character")
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(hP), []byte(pP))
+
+	if err != nil {
+		return errors.New("Password doesn't match")
+	}
+
+	return nil
+
+}
+
+func (a Auth) VerifyToken(t string) (domain.User, error) {
+	tokenArr := strings.Split(t, " ")
+	if len(tokenArr) != 2 {
+		return domain.User{}, nil
+	}
+
+	tokenStr := tokenArr[1]
+
+	if tokenArr[0] != "Bearer" {
+		return domain.User{}, errors.New("invalid token")
+	}
+
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unknown signing method %v", token.Header)
+		}
+		return []byte(a.Secret), nil
+	})
+
+	if err != nil {
+		return domain.User{}, errors.New("invalid signing method")
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			return domain.User{}, errors.New("token is expired")
+		}
+
+		user := domain.User{}
+		user.ID = uint(claims["user_id"].(float64))
+		user.Email = claims["email"].(string)
+		user.UserType = claims["role"].(string)
+		return user, nil
+	}
+
+	return domain.User{}, errors.New("token verification failed")
+}
+func (a Auth) Authorize(crx *fiber.Ctx) error {
+	return nil
+
+}
+
+func (a Auth) GetCurrentUser(ctx *fiber.Ctx) domain.User {
+	return domain.User{}
+
+}
